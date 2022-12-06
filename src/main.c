1dde0385fbf2e97586f1a781e5fe0e6aa5ee17a7
RaindropZheng <zhengyudi@ufl.edu>
2022-12-06 17:07:14 -0500

    original files

--- src/main.c
+++ src/main.c
@@ -7,15 +7,15 @@
 // Debug
 #include "esp_log.h"
 
-const char* Wifi_SSID = "ATTMfTv66a";
-const char* Wifi_Pass = "wfd97s#fg+gx";
+const char* Wifi_SSID = "RaindropZ"; //Shuhao's Galaxy Z Fold3 5G
+const char* Wifi_Pass = "Raindropy";    //sils7884
 
 // AWS Stuff
 #define CONFIG_AWS_IOT_MQTT_TX_BUF_LEN 100
 #define CONFIG_AWS_IOT_MQTT_RX_BUF_LEN 100
-#define CONFIG_AWS_IOT_MQTT_NUM_SUBSCRIBE_HANDLERS 4
+#define CONFIG_AWS_IOT_MQTT_NUM_SUBSCRIBE_HANDLERS i
 
-const char* Mqtt_Broker_Url  = "mqtts://a2m733ysclyt2b-ats.iot.us-east-2.amazonaws.com";
+const char* Mqtt_Broker_Url  = "mqtts://a2wfga7d77qb9w-ats.iot.us-east-2.amazonaws.com";
 // Host object
 host_t host;
 void send_env_data(host_t* host);
@@ -54,9 +54,9 @@ void send_env_data(host_t* host)
   }
 
   xQueuePeek(host->htu21.msg_queue, &env_data, 5);
-  sprintf(buff, "{\"temperature\": %.2f, \"humidity\": %.2f}",
-    env_data.temperature, env_data.humidity);
-  msg_id = esp_mqtt_client_publish(host->mqtt_client, "/topic/env_data",
+  sprintf(buff,"Temputer:%.2f F\n Humidity:%.2f %%", env_data.temperature,env_data.humidity);
+  //sprintf(buff, "{\"Temperature\": %.2f, \"Humidity\": %.2f}",env_data.temperature, env_data.humidity);
+  msg_id = esp_mqtt_client_publish(host->mqtt_client, "Topic/env_data",
     buff, 0, 1, 0);
   LOG_PRINTF("Sent publish successful, msg_id=%d", msg_id);
 }
